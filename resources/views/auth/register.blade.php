4e7a0106236e04413861b18ebc08ea0d10ab2109
mutebi nuhu <mutebinuhu1@gmail.com>
2020-02-28 07:46:05 +0000

    making some changes to register page

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,10 +4,10 @@
 <div class="container">
    <div class="row justify-content-center">
       <div class="col-md-8">
-         <div class="card">
+         <div class="card form">
             <div class="card-header">{{ __('Register') }}</div>
             <div class="card-body">
-               <form method="POST" action="{{ route('register') }}" autocomplete="off" class="form">
+               <form method="POST" action="{{ route('register') }}" autocomplete="off" >
                   @csrf
                   <div class="form-row">
                      <div class="form-group col-md-6">
