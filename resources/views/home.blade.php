a225f0159f65b3612792e7da4e18f7f6e8bb500a
mutebi nuhu <mutebinuhu1@gmail.com>
2020-02-29 09:34:04 +0000

    working on the home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,18 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    hi {{Auth::User()->sur_name}}
-                </div>
-            </div>
+    <div class="container home-container">
+        <!--<h1>Welcome <span style="color: #FFCB9A;">{{Auth::User()->sur_name}}</span></h1>-->
+        <div class="welcome-text">
+            <h1 class="hello"><span style="font-size: 50px; color: #D1EBE2; font-family:consolas">hi</span>{{Auth::user()->sur_name}}</h1>
         </div>
+   
+    <div class="welcome-text-2">
+        <h3 class="hello-3">Thanks For signingup, otherwise hold on as we set up things for you</h3>
+        <br>
     </div>
-</div>
+    <div class="welcome-text-3">
+        <h3>Click At The Top right To Logout</h3>
+    </div>
+    </div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,8 +17,9 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+    
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
     <style type="text/css">
         .bord-btm{
