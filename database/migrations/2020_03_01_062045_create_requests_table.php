ada7d92e1fc5ac2c9858c9128609f0cb2a4cc08e
mutebi nuhu <mutebinuhu1@gmail.com>
2020-03-01 06:30:07 +0000

    creating requests table

---
+++ database/migrations/2020_03_01_062045_create_requests_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRequestsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('requests', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->longText('description');
+            $table->string('created_by');
+            $table->string('updated_by');
+            $table->string('status')->default('default');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('requests');
+    }
+}
