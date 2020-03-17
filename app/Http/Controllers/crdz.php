3d8be913ca04ea8bb6e023f7f05677e79af450d3
mutebi nuhu <mutebinuhu1@gmail.com>
2020-03-17 02:25:33 +0000

    adding dashboard layout

--- app/Http/Controllers/crdz.php
+++ app/Http/Controllers/crdz.php
@@ -2,7 +2,7 @@
 
 
 
-Server Account Credentials
+$p "Server Account Credentials
 ----------------------------
 IP:172.104.217.250
 username:nuhu
@@ -13,6 +13,7 @@ Database credentials
 username:fleetadmin
 password:!db_CML123!#
 
+email:mutebinuhu1@gmail.com
 
-
+"
  ?>
--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,22 +1,7 @@
 @extends('layouts.dashboard')
 @section('content')
-<div class="content-header">
-      <div class="container-fluid">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1 class="m-0 text-dark">Dashboard</h1>
-          </div><!-- /.col -->
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active">Dashboard v1</li>
-            </ol>
-          </div><!-- /.col -->
-        </div><!-- /.row -->
-      </div><!-- /.container-fluid -->
-    </div>
     <div class="row content">
-          <div class="col-lg-12 col-12">
+          <div class="col-lg-12 col-md-12">
             <!-- small box -->
             <div class="small-box bg-info">
               <div class="inner">
