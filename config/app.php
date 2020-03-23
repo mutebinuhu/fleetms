bb9cb2623a1756d9950cf0de3f51bc4bc48652c4
mutebi nuhu <mutebinuhu1@gmail.com>
2020-03-23 09:33:59 +0000

    still datatables

--- config/app.php
+++ config/app.php
@@ -162,8 +162,8 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-        yajra\Datatables\DatatablesServiceProvider::class,
-        
+        yajra\DataTables\DataTablesServiceProvider::class,
+
 
         /*
          * Package Service Providers...
@@ -228,7 +228,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'Datatables' => yajra\Datatables\Facades\Datatables::class,
+        'Datatables' => yajra\DataTables\Facades\DataTables::class,
 
 
 
